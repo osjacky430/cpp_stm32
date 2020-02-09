1acef91560e58efd6c145cb25201a56700c53977
arithemetica <osjacky430@gmail.com>
2020-02-10 02:29:36 +0800

    Add examples

---
+++ example/blink/README.md
@@ -0,0 +1,2 @@
+## blink (Under Construction)
+- Tested on STM32-NUCLEO-F446, STM32-NUCLEO-L432
---
+++ example/blink/ll_driver.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2020 by osjacky430.
+// All Rights Reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the Lesser GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// Lesser GNU General Public License for more details.
+//
+// You should have received a copy of the Lesser GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "cpp_stm32/target/stm32/f4/gpio.hxx"
+#include "cpp_stm32/target/stm32/f4/rcc.hxx"
+
+constexpr void setup_led_gpio() noexcept {
+	using namespace cpp_stm32::gpio;
+	using namespace cpp_stm32::rcc;
+
+	enable_periph_clk<PeriphClk::GpioA>();
+	mode_setup<Port::PortA, Pin::Pin5>(Mode::Output, Pupd::None);
+}
+
+int main() {
+	using namespace cpp_stm32::gpio;
+
+	while (1) {
+		constexpr auto SOME_INTERVAL = 100000;
+		for (int i = 0; i < SOME_INTERVAL; ++i) {
+			__asm("nop");
+		}
+
+		toggle<Port::PortA, Pin::Pin5>();
+	}
+}
